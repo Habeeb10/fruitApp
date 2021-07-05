9c8b5b4e45fda2acbfb1ee6280db9480f1ea2e14
Tijani Habeeb <79145763+habeeb10@users.noreply.github.com>
2021-07-05 22:52:30 +0100

    refactor

--- src/screens/basket/basket.js
+++ src/screens/basket/basket.js
@@ -3,10 +3,11 @@ import React, { useState } from "react";
 import { Image, SafeAreaView, Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Basketstyles as styles } from "../basket/utils/styles";
-import { Add, Hart, Sub, Toback } from "../../../assets/svg";
+import { Hart, Toback } from "../../../assets/svg";
 import { Value } from "./utils/valuecomponent";
 import { Button } from "../../shared/button";
 import { Header } from "../../shared/header";
+import { Container } from "../../common/Container";
 
 export default function Basket({ navigation, route }) {
   const { details } = route.params;
@@ -14,48 +15,45 @@ export default function Basket({ navigation, route }) {
   const icon = getImage(title);
 
   return (
-    <SafeAreaView style={{ backgroundColor: "orange", height: "40%" }}>
-      <Header
-        icon={<Toback />}
-        back="Go back"
-        onPress={() => navigation.navigate("home")}
-      />
-      <Image source={icon} style={styles.image} />
-
-      <StatusBar style={"light"} />
-
-      <View style={styles.cont}>
-        <Text style={styles.title}>{title}</Text>
-        <View style={styles.box2}>
-          <Value />
-          <Text style={styles.amount}>{`# ${amount}`}</Text>
-        </View>
-        <View style={styles.line}></View>
-        <View style={styles.packbox}>
-          <Text style={styles.pack}>One Pack Contains:</Text>
-          <View style={styles.lines}></View>
-          <Text style={styles.mint}>
-            Red Quinoa, Lime, Honey, Blueberries, Strawberries, Mango, Fresh
-            mint.
-          </Text>
-          <View style={styles.line}></View>
-        </View>
-        <Text style={styles.brunch}>
-          If you are looking for a new fruit salad to eat today, quinoa is the
-          perfect brunch for you. make
-        </Text>
-        <View style={styles.addtocart}>
-          <View style={styles.hartbox}>
-            <Hart />
-          </View>
-          <Button
-            title="Add to basket"
-            style={styles.button}
-            onPress={() => navigation.navigate("order")}
-          />
-        </View>
+    <Container>
+      <View style={{ backgroundColor: "orange", height: "35%" }}>
+        <Header
+          icon={<Toback />}
+          back="Go back"
+          onPress={() => navigation.navigate("home")}
+        />
+        <Image source={icon} style={styles.image} />
       </View>
-    </SafeAreaView>
+
+      <Text style={styles.title}>{title}</Text>
+      <View style={styles.box2}>
+        <Value />
+        <Text style={styles.amount}>{`# ${amount}`}</Text>
+      </View>
+      <View style={styles.line}></View>
+      <View style={styles.packbox}>
+        <Text style={styles.pack}>One Pack Contains:</Text>
+        <View style={styles.lines}></View>
+        <Text style={styles.mint}>
+          Red Quinoa, Lime, Honey, Blueberries, Strawberries, Mango, Fresh mint.
+        </Text>
+        <View style={styles.line}></View>
+      </View>
+      <Text style={styles.brunch}>
+        If you are looking for a new fruit salad to eat today, quinoa is the
+        perfect brunch for you. make
+      </Text>
+      <View style={styles.addtocart}>
+        <View style={styles.hartbox}>
+          <Hart />
+        </View>
+        <Button
+          title="Add to basket"
+          style={styles.button}
+          onPress={() => navigation.navigate("order")}
+        />
+      </View>
+    </Container>
   );
 }
 
