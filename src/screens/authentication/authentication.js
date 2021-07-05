399982273a8cb11523ab7b45fa539412f3b2dca9
Tijani Habeeb <79145763+habeeb10@users.noreply.github.com>
2021-07-05 22:52:26 +0100

    refactor

--- src/screens/authentication/authentication.js
+++ src/screens/authentication/authentication.js
@@ -7,25 +7,27 @@ import Ellipse from "../../../assets/svg";
 import { drops } from "../../../assets/images";
 import { Button } from "../../shared/button";
 import { Fillinput } from "../../shared/input";
+import { Container } from "../../common/Container";
 
 export default function Authentication({ navigation }) {
   return (
-    <SafeAreaView style={{ backgroundColor: "orange" }}>
-      <StatusBar style={"dark"} />
-      <Image source={drops} style={styles.drops} />
-      <Image source={fruitbascket} style={styles.basket} />
-      <View style={styles.ellipse}>
-        <Ellipse />
+    <Container>
+      <View style={{ backgroundColor: "orange", height: "63%" }}>
+        <Image source={drops} style={styles.drops} />
+        <Image source={fruitbascket} style={styles.basket} />
+        <View style={styles.ellipse}>
+          <Ellipse />
+        </View>
+        <View style={styles.cont}>
+          <Text style={styles.combo}>What is your firstname?</Text>
+          <Fillinput placeholder="Tony" style={{ marginTop: 20 }} />
+          <Button
+            title="Start Ordering"
+            style={{ marginTop: 30 }}
+            onPress={() => navigation.navigate("home")}
+          />
+        </View>
       </View>
-      <View style={styles.cont}>
-        <Text style={styles.combo}>What is your firstname?</Text>
-        <Fillinput placeholder="Tony" style={{ marginTop: 20 }} />
-        <Button
-          title="Start Ordering"
-          style={{ marginTop: 30 }}
-          onPress={() => navigation.navigate("home")}
-        />
-      </View>
-    </SafeAreaView>
+    </Container>
   );
 }
