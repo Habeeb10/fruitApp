dd04a69832c696eb1c7d62b9d3a6c39751fa20c3
Tijani Habeeb <79145763+habeeb10@users.noreply.github.com>
2021-07-05 13:06:11 +0100

    feat:added svg and png images

---
+++ assets/images/index.js
@@ -0,0 +1,8 @@
+export const bascket = require("./bascket.png");
+export const fruitbascket = require("./fruitbascket.png");
+export const berry = require("./berry.png");
+export const breakfast = require("./breakfast.png");
+export const glowing = require("./glowing.png");
+export const peach = require("./peach.png");
+export const tropical = require("./tropical.png");
+export const drops = require("./drops.png");
