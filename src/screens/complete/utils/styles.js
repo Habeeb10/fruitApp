fe12a1b4c4369391f0bc6cc84cb72440766c3bfb
Tijani Habeeb <79145763+habeeb10@users.noreply.github.com>
2021-07-05 22:52:14 +0100

    refactor

---
+++ src/screens/complete/utils/styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+import * as colors from "../../../common/color";
+
+export const Completestyles = StyleSheet.create({
+  checkbox: {
+    width: 164,
+    height: 164,
+    borderWidth: 1,
+    borderColor: colors.green,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 100,
+  },
+  checkbox2: {
+    width: 100,
+    height: 100,
+    borderWidth: 1,
+    backgroundColor: colors.green,
+  },
+});
