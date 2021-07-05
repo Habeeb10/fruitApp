38df7a6871bcd0833af5c3d68cfcd2b62f08979f
Tijani Habeeb <79145763+habeeb10@users.noreply.github.com>
2021-07-05 13:10:49 +0100

    feat: order screen

---
+++ src/screens/order/utils/ordercomponents.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { View } from "react-native";
+import * as Images from "../../../../assets/images";
+import { Orderstyles as styles } from "./styles";
+import { OrderCard } from "./ordercard";
+
+const Orderlist = [
+  {
+    icon: Images.breakfast,
+    title: "Quinoa fruit salad",
+    packs: "2packs",
+    amount: "20,000",
+  },
+
+  {
+    icon: Images.tropical,
+    title: "Tropical fruit salad",
+    packs: "2packs",
+    amount: "20,000",
+  },
+
+  {
+    icon: Images.berry,
+    title: "melon fruit salad",
+    packs: "2packs",
+    amount: "20,000",
+  },
+];
+
+export const Ordercomp = () => {
+  return (
+    <View style={styles.orderbox}>
+      {Orderlist.map((item, index) => {
+        return (
+          <OrderCard
+            key={index}
+            icon={item.icon}
+            title={item.title}
+            packs={item.packs}
+            amount={item.amount}
+          />
+        );
+      })}
+    </View>
+  );
+};
