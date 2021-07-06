c5af02d46a5eeb311a44482b9f972fca70701097
Tijani Habeeb <79145763+habeeb10@users.noreply.github.com>
2021-07-06 10:56:54 +0100

    feat: added modal component

--- src/screens/order/utils/modalcomponent.js
+++ src/screens/order/utils/modalcomponent.js
@@ -5,8 +5,9 @@ import { Orderstyles as styles } from "./styles";
 import { Button } from "../../../shared/button";
 import Modal from "react-native-modal";
 import { Labelinput } from "../../../shared/labelinput";
+import { useNavigation } from "@react-navigation/core";
 
-export const Modalcomponent = ({ close, isVisible }) => {
+export const Modalcomponent = ({ close, isVisible, handleDelivery }) => {
   return (
     <View>
       <Modal
@@ -28,13 +29,7 @@ export const Modalcomponent = ({ close, isVisible }) => {
               <TouchableOpacity style={styles.paybox}>
                 <Text style={styles.pay}>Pay on delivery</Text>
               </TouchableOpacity>
-              <TouchableOpacity
-                style={styles.paybox}
-                onPress={() => {
-                  setModalState1(false);
-                  setModalState2(true);
-                }}
-              >
+              <TouchableOpacity style={styles.paybox} onPress={handleDelivery}>
                 <Text style={styles.pay}>Pay with card</Text>
               </TouchableOpacity>
             </View>
@@ -45,33 +40,45 @@ export const Modalcomponent = ({ close, isVisible }) => {
   );
 };
 
-export const Modalcomponent2 = ({ close, isVisible }) => {
+export const Modalcomponent2 = ({ isVisible, close }) => {
+  const navigation = useNavigation();
+
   return (
-    <Modal
-      visible={isVisible}
-      style={{ marginHorizontal: 0, backgroundColor: "rgba(0,0,0,.3)" }}
-      coverScreen
-    >
-      <View style={{ position: "absolute", right: 0, left: 0, bottom: 42 }}>
-        <TouchableOpacity style={styles.exitbox} onPress={close}>
-          <Exit />
-        </TouchableOpacity>
-        <View style={styles.modalBox2}>
-          <Labelinput label="Card Holders Name" placeHolder="Adolphus Chris" />
-          <Labelinput label="Card Number" placeHolder="1234 5678 9012 1314" />
-          <View style={styles.cardcont}>
-            <Labelinput label="Date" placeHolder="10/30" style={styles.date} />
-            <Labelinput label="CCV" placeHolder="123" style={styles.ccv} />
-          </View>
-          <View style={styles.butbox}>
-            <Button
-              style={styles.ordercont}
-              title="Complete Order"
-              titleStyle={styles.title}
+    <View>
+      <Modal
+        visible={isVisible}
+        style={{ marginHorizontal: 0, backgroundColor: "rgba(0,0,0,.3)" }}
+        coverScreen
+      >
+        <View style={{ height: 180, bottom: 0, left: 0, right: 0 }}>
+          <TouchableOpacity style={styles.exitbox} onPress={close}>
+            <Exit />
+          </TouchableOpacity>
+          <View style={styles.modalBox2}>
+            <Labelinput
+              label="Card Holders Name"
+              placeHolder="Adolphus Chris"
             />
+            <Labelinput label="Card Number" placeHolder="1234 5678 9012 1314" />
+            <View style={styles.cardcont}>
+              <Labelinput
+                label="Date"
+                placeHolder="10/30"
+                style={styles.date}
+              />
+              <Labelinput label="CCV" placeHolder="123" style={styles.ccv} />
+            </View>
+            <View style={styles.butbox}>
+              <Button
+                style={styles.ordercont}
+                title="Complete Order"
+                titleStyle={styles.title}
+                onPress={() => navigation.navigate("complete")}
+              />
+            </View>
           </View>
         </View>
-      </View>
-    </Modal>
+      </Modal>
+    </View>
   );
 };
